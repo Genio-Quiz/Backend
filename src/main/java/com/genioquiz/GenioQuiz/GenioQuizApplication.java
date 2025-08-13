1dfebf318312002130e28a78e7b63975b98082c7
aluno <etecae023@hotmail.com>
2025-08-13 09:51:06 -0300

    first commit :tada:

---
+++ src/main/java/com/genioquiz/GenioQuiz/GenioQuizApplication.java
@@ -0,0 +1,13 @@
+package com.genioquiz.GenioQuiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenioQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenioQuizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/genioquiz/GenioQuiz/GenioQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genioquiz.GenioQuiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenioQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
