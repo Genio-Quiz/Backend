f1dc86701613b71126712ff7ed0fb29b5a42c261
matheus-kiyoshi <matheus.kiyoshi.f@gmail.com>
2025-08-27 15:24:11 -0300

    feat: user functions improved

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, Param } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './user.entity';
 
@@ -6,16 +6,20 @@ import { User } from './user.entity';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @HttpCode(HttpStatus.OK)
   @Get()
   async findAll(): Promise<User[]> {
     return this.userService.findAll();
   }
 
+  @HttpCode(HttpStatus.OK)
   @Get(':id')
   async findById(@Param('id') id: number): Promise<User | null> {
     return this.userService.findByOneId(Number(id));
   }
-  @Get(':username')
+
+  @HttpCode(HttpStatus.OK)
+  @Get('/search/:username')
   async findByUsername(
     @Param('username') username: string,
   ): Promise<User | null> {
