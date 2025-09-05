9b47e7b204dc427c7859b5711c76065e4e6b29bc
leofesteves <estevesleonardo01@gmail.com>
2025-09-05 11:57:06 -0300

    feat: Configuring Nodemailer

---
+++ src/mail/mail.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { MailService } from './mail.service';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MailerModule.forRoot({
+      transport: {
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+          user: process.env.GMAIL_USER,
+          pass: process.env.GMAIL_PASS,
+        },
+      },
+      defaults: {
+        from: `"Meu App" <${process.env.GMAIL_USER}>`,
+      },
+    }),
+  ],
+  providers: [MailService],
+  exports: [MailService],
+})
+export class MailModule {}
